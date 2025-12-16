b4e0c641d8a930b21cb6515b4325e9c2001a02ee
portariasibge <adriel.ibge@gmail.com>
2025-12-16 13:57:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -299,4 +299,6 @@ def gerar_portaria_gsiste_lote_endpoint():
 
 # --- Execução da Aplicação ---
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
